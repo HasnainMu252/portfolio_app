1e0c42d02cdc9740ac39123f4b0c439cc87fbdb8
Hasnain <hasnain252@gmail.com>
2024-10-20 22:31:30 +0500

    1st UI

---
+++ app/src/main/java/com/portfolio_app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.portfolio_app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Adapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var RecyclerRepo : RecyclerView
+    lateinit var adapter : Adapter
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        RecyclerRepo = findViewById<RecyclerView>(R.id.Repolist)
+
+
+
+
+
+
+    }
+}
